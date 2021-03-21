0ca92d69a3be0438ab00657b0d996eda621f2111
Solayman-Emon <emonqt19@gmail.com>
2021-03-21 14:24:53 +0600

    Add project files.

---
+++ Demo_App/Demo_App/ImageResourceEntension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Embedded_Images
+{
+    [ContentProperty (nameof(Source)) ]
+    class  ImageResourceEntension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+           if(Source == null)
+            {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceEntension).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
---
+++ Demo_App/Demo_App/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            coverImage.Source = ImageSource.FromResource("Demo_App.images.brain.png");
+            projectImage.Source = ImageSource.FromResource("Demo_App.images.img.png");
+        }
+    }
+}
